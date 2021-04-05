f2350055154c9e6e3b52f6794f9332022f78a684
Kheengmasu <81807620+Kheengmasu@users.noreply.github.com>
2021-04-05 11:40:18 +0100

    Add files via upload

---
+++ Csc104assignment-1.py
@@ -0,0 +1,32 @@
+'''Write a short Python code to display the name of your name & department'''
+name = "Shuaibu Ahmed"
+department = "Computer science"
+print (name)
+print (department)
+'''Assign the string "Boo!" to the variable scare and print the variable'''
+scare = "Boo!"
+print (scare)
+'''Add two variables and assign the result to a third variable. print all the variable names'''
+a = 8
+b = 7
+c = (a + b)
+print (c)
+'''Divide 12 by the decimal .5 and assign the result to the variable outcome and print the variable'''
+division = 12/.5
+outcome = (division)
+print (division)
+'''Assign the sum of two numbers to a variable and print the variable'''
+number1 = 10
+number2 = 6
+number3 = (number1 + number2)
+print (number3)
+'''Write the code that finds the remainder when 8 is divided by 3'''
+modulus1 = 8
+modulus2 = 3
+modulus3 = (modulus1 % modulus2)
+print (modulus3)
+'''Rewrite the following statement to force this order: First, multiply 2 by 4. Then add 4 and 2. Then multiply the first result by the second result. Print the result'''
+order1 = 2 * 4
+order2 = 4 + 2
+order3 = (order1 * order2)
+print (order3)
